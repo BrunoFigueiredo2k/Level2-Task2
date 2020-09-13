a28dcb76be9f114768ee151d667ba4fc1ebf7d94
@BrunoFigueiredo2k <brunofigueiredo@live.nl>
2020-09-13 21:50:10 +0200

    Fully working (added functionality correct or incorrect answer in MainActivity)

--- app/src/main/java/com/example/level2_task2/MainActivity.kt
+++ app/src/main/java/com/example/level2_task2/MainActivity.kt
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.*
 import com.example.level2_task2.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.question.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val questions = arrayListOf<Question>() // Done to get m
+    private val questions = arrayListOf<Question>()
     private val questionAdapter = QuestionAdapter(questions)
     private lateinit var binding: ActivityMainBinding
 
@@ -52,16 +54,27 @@ class MainActivity : AppCompatActivity() {
             // Callback triggered when a user swiped an item.
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
                 val position = viewHolder.adapterPosition
-                val answer: Boolean
+
+                var questionAnswer = ""
+                when (position){
+                    0 -> questionAnswer = "False"
+                    1 -> questionAnswer = "True"
+                    2 -> questionAnswer = "True"
+                    3 -> questionAnswer = "True"
+                }
+                
+                // Check if user answer is equal to answer in object
+                if (direction == 8 && questionAnswer == "True" || direction == 4 && questionAnswer == "False") {
+                    // Delete from list if answer is true
+                    questions.removeAt(position)
+                } else {
+                    // Snackbar message incorrect answer
+                    Snackbar.make(tvQuestion, "The answer is incorrect, so it will not be removed from list.", Snackbar.LENGTH_SHORT).show()
+                }
 
                 questionAdapter.notifyDataSetChanged()
             }
         }
         return ItemTouchHelper(callback)
     }
-
-    private fun checkAnswer(question: Question, answer: Question){
-
-    }
-
 }
